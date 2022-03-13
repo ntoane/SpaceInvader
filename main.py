37229ee67eb6b6ed8adeeee6cd297c6c2cecc419
Lekuba Ntoane <l.ntoane@gmail.com>
2022-03-13 22:33:04 +0200

    Adding Player image

--- main.py
+++ main.py
@@ -12,14 +12,29 @@ pygame.display.set_caption("Space Invaders")
 icon = pygame.image.load("images/icon.png")
 pygame.display.set_icon(icon)
 
+# Add the player image and position it on the screen
+playerImg = pygame.image.load("images/player.png")
+playerX = 370
+playerY = 480
+
+
+# Function to draw (using blit function) image on the screen
+def player():
+    screen.blit(playerImg, (playerX, playerY))
+
+
 # Access a QUIT event and close the window when it is clicked
 windowRunning = True
 while windowRunning:  # we can access QUIT event when the window is running
+    # Background colour
+    screen.fill((0, 0, 0))
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Change the boolean to false so that the while loop can stop
             windowRunning = False
 
-    # Background colour
-    screen.fill((0, 0, 0))
+    # After filling the screen with background color, draw the player image
+    player()
+
     pygame.display.update()
