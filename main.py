af4f1fdd5b6967ad61d15a75aaeb4e3c69d68a47
Lekuba Ntoane <l.ntoane@gmail.com>
2022-03-13 22:00:33 +0200

    Changing the Title, Logo and Background Color

--- main.py
+++ main.py
@@ -6,6 +6,12 @@ pygame.init()
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
 
+# Title and Icon
+pygame.display.set_caption("Space Invaders")
+# 32 Pixel PNG image recommended
+icon = pygame.image.load("images/icon.png")
+pygame.display.set_icon(icon)
+
 # Access a QUIT event and close the window when it is clicked
 windowRunning = True
 while windowRunning:  # we can access QUIT event when the window is running
@@ -13,3 +19,7 @@ while windowRunning:  # we can access QUIT event when the window is running
         if event.type == pygame.QUIT:
             # Change the boolean to false so that the while loop can stop
             windowRunning = False
+
+    # Background colour
+    screen.fill((0, 0, 0))
+    pygame.display.update()
