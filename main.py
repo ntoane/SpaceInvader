e6f124db5a1f3ea226d026680db683f37ebe7342
Lekuba Ntoane <l.ntoane@gmail.com>
2022-03-13 23:12:10 +0200

    Keyboard Input Controls & Key Pressed Event

--- main.py
+++ main.py
@@ -16,25 +16,40 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load("images/player.png")
 playerX = 370
 playerY = 480
+playerX_change = 0
 
 
 # Function to draw (using blit function) image on the screen
-def player():
-    screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+    screen.blit(playerImg, (x, y))
 
 
-# Access a QUIT event and close the window when it is clicked
+# Loop of the Game
 windowRunning = True
 while windowRunning:  # we can access QUIT event when the window is running
     # Background colour
     screen.fill((0, 0, 0))
 
+    # Access a QUIT event and close the window when it is clicked
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             # Change the boolean to false so that the while loop can stop
             windowRunning = False
 
+        # If keystroke is pressed check whether it is left or right keystroke
+        if event.type == pygame.KEYDOWN:  # if any keystroke is pressed down
+            if event.key == pygame.K_LEFT:  # then if the key is LEFT arrow, move player to Left by 0.1
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:  # then if the key is RIGHT arrow, move player to Right by 0.1
+                playerX_change = 0.3
+
+        if event.type == pygame.KEYUP:  # if any keystroke pressed is released
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                # When either Left or Right key released, stop the X coordinate and maintain that position
+                playerX_change = 0
+
     # After filling the screen with background color, draw the player image
-    player()
+    playerX += playerX_change  # Change the X coordinate
+    player(playerX, playerY)
 
     pygame.display.update()
