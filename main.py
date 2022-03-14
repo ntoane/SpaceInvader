59a34681c05c1c8ca7d4ac05824e1224b5a4f976
Lekuba Ntoane <l.ntoane@gmail.com>
2022-03-14 23:43:58 +0200

    Background image

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 
 # Create the screen
 screen = pygame.display.set_mode((800, 600))
+# Load background image
+backgroundImg = pygame.image.load("images/bg.png")
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
@@ -23,7 +25,7 @@ playerX_change = 0
 enemyImg = pygame.image.load("images/enemy.png")
 enemyX = random.randint(0, 736)
 enemyY = random.randint(50, 150)
-enemyX_change = 0.3
+enemyX_change = 1.5
 enemyY_change = 40
 
 
@@ -33,7 +35,7 @@ def player(x, y):
 
 
 def enemy(x, y):
-    screen.blit(enemyImg, (enemyX, enemyY))
+    screen.blit(enemyImg, (x, y))
 
 
 # Checking for boundaries of spaceship so that it does not go out of bound
@@ -51,10 +53,10 @@ def enemyBoundary():
     global enemyY
     global enemyX_change
     if enemyX <= 0:
-        enemyX_change = 0.3
+        enemyX_change = 1.5
         enemyY += enemyY_change
     elif enemyX >= 736:
-        enemyX_change = -0.3
+        enemyX_change = -1.5
         enemyY += enemyY_change
 
 
@@ -63,6 +65,8 @@ windowRunning = True
 while windowRunning:  # we can access QUIT event when the window is running
     # Background colour
     screen.fill((0, 0, 0))
+    # Add background image
+    screen.blit(backgroundImg, (0, 0))
 
     # Access a QUIT event and close the window when it is clicked
     for event in pygame.event.get():
@@ -73,9 +77,9 @@ while windowRunning:  # we can access QUIT event when the window is running
         # If keystroke is pressed check whether it is left or right keystroke
         if event.type == pygame.KEYDOWN:  # if any keystroke is pressed down
             if event.key == pygame.K_LEFT:  # then if the key is LEFT arrow, move player to Left by 0.1
-                playerX_change = -0.3
+                playerX_change = -2
             if event.key == pygame.K_RIGHT:  # then if the key is RIGHT arrow, move player to Right by 0.1
-                playerX_change = 0.3
+                playerX_change = 2
 
         if event.type == pygame.KEYUP:  # if any keystroke pressed is released
             if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
